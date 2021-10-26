3ce7631cf75fb414a47ccce481fcad0f60b051f5
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 22:41:37 +0200

    Create the mvie component.

---
+++ src/components/movies.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+export default class Movies extends Component {
+	state = {
+		movies: getMovies()
+	};
+
+	handleDelete = (movie) => {
+		const movies = this.state.movies.filter((m) => m._id !== movie._id);
+		this.setState({ movies });
+	};
+
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th scope="col">Title</th>
+						<th scope="col">Genre</th>
+						<th scope="col">Stock</th>
+						<th scope="col">Rate</th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.movies.map((movie) => (
+						<tr key={movie._id}>
+							<td>{movie.title}</td>
+							<td>{movie.genre.name}</td>
+							<td>{movie.numberInStock}</td>
+							<td>{movie.dailyRentalRate}</td>
+							<td>
+								<button
+									onClick={() => this.handleDelete(movie)}
+									className="btn btn-danger"
+								>
+									delete
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
+}
