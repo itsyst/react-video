423ce8954d0776ee4c923ecd4c15277774e35cbf
Khaled El Hamzi <contact@elhamzi.me>
2021-10-26 22:59:45 +0200

    Add a conditional rendering.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,36 +12,44 @@ export default class Movies extends Component {
 	};
 
 	render() {
+		const { length: count } = this.state.movies;
+
+		if (count === 0)
+			return <p className="py-3">There is no movies in the database.</p>;
+
 		return (
-			<table className="table table-hover">
-				<thead>
-					<tr>
-						<th scope="col">Title</th>
-						<th scope="col">Genre</th>
-						<th scope="col">Stock</th>
-						<th scope="col">Rate</th>
-						<th scope="col"></th>
-					</tr>
-				</thead>
-				<tbody>
-					{this.state.movies.map((movie) => (
-						<tr key={movie._id}>
-							<td>{movie.title}</td>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>{movie.dailyRentalRate}</td>
-							<td>
-								<button
-									onClick={() => this.handleDelete(movie)}
-									className="btn btn-danger"
-								>
-									delete
-								</button>
-							</td>
+			<>
+				<p>Showing {count} in the database.</p>
+				<table className="table table-hover">
+					<thead>
+						<tr>
+							<th scope="col">Title</th>
+							<th scope="col">Genre</th>
+							<th scope="col">Stock</th>
+							<th scope="col">Rate</th>
+							<th scope="col"></th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{this.state.movies.map((movie) => (
+							<tr key={movie._id}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td>{movie.numberInStock}</td>
+								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<button
+										onClick={() => this.handleDelete(movie)}
+										className="btn btn-danger"
+									>
+										delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</>
 		);
 	}
 }
