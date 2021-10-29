87199142d0971da7e9b91613de3449aa90c57488
Khaled El Hamzi <contact@elhamzi.me>
2021-10-30 01:08:49 +0200

    Implement sort method based on columns.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,17 +5,19 @@ import Pagination from './common/pagination';
 import { getGenres } from '../services/fakeGenreService';
 import { getMovies } from '../services/fakeMovieService';
 import { paginate } from '../utils/paginate';
+import _ from 'lodash';
 
 export default class Movies extends Component {
 	state = {
 		movies: [],
 		genres: [],
 		currentPage: 1,
-		pageSize: 4
+		pageSize: 4,
+		sortColumn: { path: 'title', order: 'asc' }
 	};
 
 	componentDidMount = () => {
-		const uid = Date.now().toString(32) +Math.random();
+		const uid = Date.now().toString(32) + Math.random();
 		const genres = [{ _id: uid, name: 'All Genres' }, ...getGenres()];
 		this.setState({ movies: getMovies(), genres: genres });
 	};
@@ -41,6 +43,10 @@ export default class Movies extends Component {
 		this.setState({ selectedGenre: genre, currentPage: 1 });
 	};
 
+	handleSort = (sortColumn) => {
+		this.setState({ sortColumn });
+	};
+
 	render() {
 		const { length: count } = this.state.movies;
 		const {
@@ -48,18 +54,25 @@ export default class Movies extends Component {
 			currentPage,
 			movies: allMovies,
 			genres,
-			selectedGenre
+			selectedGenre,
+			sortColumn
 		} = this.state;
 
 		if (count === 0)
 			return <p className="py-3">There is no movies in the database.</p>;
 
 		const filtered =
-			selectedGenre && (selectedGenre.name !=='All Genres')
+			selectedGenre && selectedGenre.name !== 'All Genres'
 				? allMovies.filter((m) => m.genre._id === selectedGenre._id)
 				: allMovies;
-		
-		const movies = paginate(filtered, currentPage, pageSize);
+
+		const sorted = _.orderBy(
+			filtered,
+			[sortColumn.path],
+			[sortColumn.order]
+		);
+
+		const movies = paginate(sorted, currentPage, pageSize);
 
 		return (
 			<div className="row">
@@ -76,8 +89,10 @@ export default class Movies extends Component {
 					<p>Showing {filtered.length} movies in the database.</p>
 					<MoviesTable
 						movies={movies}
+						sortColumn={sortColumn}
 						onLike={this.handleLiked}
 						onDelete={this.handleDelete}
+						onSort={this.handleSort}
 					/>
 					<Pagination
 						itemsCount={filtered.length}
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,46 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Like from './common/like';
 
-const MoviesTable = (props) => {
-	const { movies, onDelete, onLike } = props;
- 	return (
-		<table className="table table-hover">
-			<thead>
-				<tr>
-					<th scope="col">Title</th>
-					<th scope="col">Genre</th>
-					<th scope="col">Stock</th>
-					<th scope="col">Rate</th>
-					<th />
-					<th />
-				</tr>
-			</thead>
-			<tbody>
-				{movies.map((movie) => (
-					<tr key={movie._id}>
-						<td>{movie.title}</td>
-						<td>{movie.genre.name}</td>
-						<td>{movie.numberInStock}</td>
-						<td>{movie.dailyRentalRate}</td>
-						<td>
-							<Like
-								liked={movie.liked}
-								onLiked={() => onLike(movie)}
-							/>
-						</td>
-						<td>
-							<button
-								onClick={() => onDelete(movie)}
-								className="btn btn-danger"
-							>
-								delete
-							</button>
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	);
-};
+export default class MoviesTable extends Component {
+	raiseSort(path) {
+		const sortColumn = { ...this.props.sortColumn };
+		if (sortColumn.path === path)
+			sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
+		else {
+			sortColumn.path = path;
+			sortColumn.order = 'desc';
+		}
+		this.props.onSort(sortColumn);
+	}
 
-export default MoviesTable;
+	render() {
+		const { movies, onDelete, onLike } = this.props;
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th onClick={() => this.raiseSort('title')} scope="col">
+							Title
+						</th>
+						<th
+							onClick={() => this.raiseSort('genre.name')}
+							scope="col"
+						>
+							Genre
+						</th>
+						<th
+							onClick={() => this.raiseSort('numberInStock')}
+							scope="col"
+						>
+							Stock
+						</th>
+						<th
+							onClick={() => this.raiseSort('dailyRentalRate')}
+							scope="col"
+						>
+							Rate
+						</th>
+						<th />
+						<th />
+					</tr>
+				</thead>
+				<tbody>
+					{movies.map((movie) => (
+						<tr key={movie._id}>
+							<td>{movie.title}</td>
+							<td>{movie.genre.name}</td>
+							<td>{movie.numberInStock}</td>
+							<td>{movie.dailyRentalRate}</td>
+							<td>
+								<Like
+									liked={movie.liked}
+									onLiked={() => onLike(movie)}
+								/>
+							</td>
+							<td>
+								<button
+									onClick={() => onDelete(movie)}
+									className="btn btn-danger"
+								>
+									delete
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
+}
