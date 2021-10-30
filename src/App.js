c8a70e61a688bc4a85f3d2d538a0c5315318d92e
Khaled El Hamzi <contact@elhamzi.me>
2021-10-31 01:54:26 +0200

    Register routes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import Movies from './components/movies'
+import Customers from './components/customers';
+import Movies from './components/movies';
+import MovieForm from './components/movieForm';
+import Navbar from './components/navbar';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <main className="container col-lg-8 p-3 py-md-5">
-      <Movies />
-    </main>
-  );
+	return (
+		<div>
+			<Navbar />
+			<main className="container col-lg-8 p-3 py-md-5">
+				<Switch>
+					<Route path="/movies/:id" component={MovieForm} />
+					<Route path="/movies" component={Movies} />
+					<Route path="/customers" component={Customers} />
+					<Route path="/rentals" component={Rentals} />
+					<Route path="/not-found" component={NotFound} />
+					<Redirect from="/" exact to="/movies" />
+					<Redirect to="/not-found" />
+				</Switch>
+			</main>
+		</div>
+	);
 }
 
 export default App;
