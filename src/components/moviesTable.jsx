f5a4e53eab68b8d0f6c3da915713852cf0464031
Khaled El Hamzi <contact@elhamzi.me>
2021-10-30 03:53:47 +0200

    Implement separate body table component.

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Like from './common/like';
 import TableHeader from './common/tableHeader';
+import TableBody from './common/tableBody';
+import Like from './common/like';
 
 export default class MoviesTable extends Component {
 	columns = [
@@ -8,8 +9,26 @@ export default class MoviesTable extends Component {
 		{ path: 'genre.name', label: 'Genre' },
 		{ path: 'numberInStock', label: 'Stock' },
 		{ path: 'dailyRentalRate', label: 'Rate' },
-		{ key: 'like' },
-		{ key: 'delete' }
+		{
+			key: 'like',
+			content: (movie) => (
+				<Like
+					liked={movie.liked}
+					onLiked={() => this.props.onLike(movie)}
+				/>
+			)
+		},
+		{
+			key: 'delete',
+			content: (movie) => (
+				<button
+					onClick={() => this.props.onDelete(movie)}
+					className="btn btn-danger"
+				>
+					delete
+				</button>
+			)
+		}
 	];
 	render() {
 		const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
@@ -20,30 +39,12 @@ export default class MoviesTable extends Component {
 					sortColumn={sortColumn}
 					onSort={onSort}
 				/>
-				<tbody>
-					{movies.map((movie) => (
-						<tr key={movie._id}>
-							<td>{movie.title}</td>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>{movie.dailyRentalRate}</td>
-							<td>
-								<Like
-									liked={movie.liked}
-									onLiked={() => onLike(movie)}
-								/>
-							</td>
-							<td>
-								<button
-									onClick={() => onDelete(movie)}
-									className="btn btn-danger"
-								>
-									delete
-								</button>
-							</td>
-						</tr>
-					))}
-				</tbody>
+				<TableBody
+					data={movies}
+					columns={this.columns}
+					onDelete={onDelete}
+					onLike={onLike}
+				/>
 			</table>
 		);
 	}
