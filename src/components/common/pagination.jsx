525d9d774489cd4253ec78113bfd9c6407dbc6ff
Khaled El Hamzi <contact@elhamzi.me>
2021-10-30 03:11:04 +0200

    Implement table Header in separate component.

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+
 const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+
 	const pagesCount = Math.ceil(itemsCount / pageSize);
 	if (pagesCount === 1) return null;
 	//const pages = Array.from(Array(pagesCount).keys()) ;
