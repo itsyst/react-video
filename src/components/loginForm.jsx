a39ee7040e67066d126ae2ebf1190843e135c781
Khaled El Hamzi <contact@elhamzi.me>
2021-10-31 14:28:35 +0100

    Use Refs vs autoFocus.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const LoginForm = () => {
-	return <div>Login Form</div>;
-};
+export default class LoginForm extends Component {
+	username = React.createRef();
 
-export default LoginForm;
+	componentDidMount() {
+		// this.username.current.focus();
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Call the server
+		// Save the changed
+		// Redirect the user to a different page
+		const username = this.username.current.value;
+		console.log('Submitted', username);
+	};
+	render() {
+		return (
+			<div className="container">
+				<h1>Login</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="username">Username</label>
+						<input
+							autoFocus
+							id="username"
+							// ref={this.username}
+							type="text"
+							className="form-control"
+						/>
+					</div>
+					<div className="form-group mb-3">
+						<label htmlFor="password">Password</label>
+						<input
+							id="password"
+							type="text"
+							className="form-control"
+						/>
+					</div>
+					<button className="btn btn-primary">Login</button>
+				</form>
+			</div>
+		);
+	}
+}
