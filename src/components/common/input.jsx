154d88a9ec8edb9d1322e9b4f1777a6a057c0f17
Khaled El Hamzi <contact@elhamzi.me>
2021-11-01 22:56:30 +0100

    Add a margin bottom.

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -4,7 +4,12 @@ const Input = ({ name, label, error, ...rest }) => {
 	return (
 		<div className="form-group">
 			<label htmlFor={name}>{label}</label>
-			<input {...rest} id={name} name={name} className="form-control" />
+			<input
+				{...rest}
+				id={name}
+				name={name}
+				className="form-control mb-3"
+			/>
 			{error && <div className="alert alert-danger p-1">{error}</div>}
 		</div>
 	);
