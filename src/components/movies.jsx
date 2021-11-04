9b06379e2474b080bf655367016f8438990e5856
Khaled El Hamzi <contact@elhamzi.me>
2021-11-05 00:50:45 +0100

    Consume the services.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
+
 import ListGroup from './common/listGroup';
 import MoviesTable from './moviesTable';
 import Pagination from './common/pagination';
 import SearchBox from './common/searchBox';
-import { getGenres } from '../services/fakeGenreService';
-import { getMovies, deleteMovie } from '../services/fakeMovieService';
+import { getGenres } from '../services/genreService';
+import { getMovies, deleteMovie } from '../services/movieService';
 import { paginate } from '../utils/paginate';
 import _ from 'lodash';
 import { Link } from 'react-router-dom';
@@ -20,17 +22,27 @@ export default class Movies extends Component {
 		sortColumn: { path: 'title', order: 'asc' }
 	};
 
-	componentDidMount = () => {
+	componentDidMount = async () => {
 		const uid = Date.now().toString(32) + Math.random();
-		const genres = [{ _id: uid, name: 'All Genres' }, ...getGenres()];
-		this.setState({ movies: getMovies(), genres: genres });
+		const { data } = await getGenres();
+		const genres = [{ _id: uid, name: 'All Genres' }, ...data];
+
+		const { data: movies } = await getMovies();
+		this.setState({ movies, genres });
 	};
 
-	handleDelete = (movie) => {
-		const movies = this.state.movies.filter((m) => m._id !== movie._id);
+	handleDelete = async (movie) => {
+		const originalMovies = this.state.movies;
+		const movies = originalMovies.filter((m) => m._id !== movie._id);
 		this.setState({ movies });
 
-		deleteMovie(movie._id);
+		try {
+			await deleteMovie(movie._id);
+		} catch (ex) {
+			if (ex.response && ex.response.status === 404)
+				toast.error('This movie has already been deleted.');
+			this.setState({ originalMovies });
+		}
 	};
 
 	handleLiked = (movie) => {
