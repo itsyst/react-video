1cfeb012ddecc6bf7000b5df15121eb2a291507e
Khaled El Hamzi <contact@elhamzi.me>
2021-11-04 23:25:09 +0100

    Consume sentry & toastify.

--- src/App.js
+++ src/App.js
@@ -8,28 +8,37 @@ import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
 import Rentals from './components/rentals';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
 import './App.css';
+
 
 function App() {
 	return (
-		<div>
-			<ToastContainer />
-			<Navbar />
-			<main className="container col-lg-8 p-3 py-md-5">
-				<Switch>
-					<Route path="/login" component={LoginForm} />
-					<Route path="/register" component={RegisterForm} />
-					<Route path="/movies/:id" component={MovieForm} />
-					<Route path="/movies" component={Movies} />
-					<Route path="/customers" component={Customers} />
-					<Route path="/rentals" component={Rentals} />
-					<Route path="/not-found" component={NotFound} />
-					<Redirect from="/" exact to="/movies" />
-					<Redirect to="/not-found" />
-				</Switch>
-			</main>
-		</div>
-	);
+		<>
+			<Sentry.ErrorBoundary
+				beforeCapture={(scope) => {
+					scope.setTag("location", "first");
+					scope.setTag("anotherTag", "anotherValue");
+				}}
+			>
+				<ToastContainer />
+				<Navbar />
+				<main className="container col-lg-8 p-3 py-md-5">
+					<Switch>
+						<Route path="/login" component={LoginForm} />
+						<Route path="/register" component={RegisterForm} />
+						<Route path="/movies/:id" component={MovieForm} />
+						<Route path="/movies" component={Movies} />
+						<Route path="/customers" component={Customers} />
+						<Route path="/rentals" component={Rentals} />
+						<Route path="/not-found" component={NotFound} />
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-found" />
+					</Switch>
+				</main>
+			</Sentry.ErrorBoundary>
+		</>
+	)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
+Sentry.init({
+  dsn: "https://33d5d43f47804a04bae0915a43580478@o408400.ingest.sentry.io/6049867",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <Router>
