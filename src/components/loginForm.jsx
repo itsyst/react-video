31c3f50d92a8ba4c018eb07c62c61e865d20854b
Khaled El Hamzi <contact@elhamzi.me>
2021-11-06 20:31:56 +0100

    Handling login errors.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -23,9 +23,9 @@ export default class LoginForm extends Form {
 			const { data } = this.state;
 			await authService.login(data.username, data.password);
 		} catch (ex) {
-			if (ex.response && ex.response.status === 401) {
+			if (ex.response && ex.response.status === 400) {
 				const errors = { ...this.state.errors };
-				errors.email = ex.response.data;
+				errors.username = ex.response.data;
 				this.setState({ errors });
 			}
 		}
